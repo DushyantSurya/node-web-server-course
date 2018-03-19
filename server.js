d43e5d95547f3b388aac350bf321b45daab5795a
Dushyant Surya <djdushyantsurya@gmail.com>
2018-03-19 20:37:18 +0530

    'Initial'

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs= require('hbs');
+const fs= require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text)=> {
+  return text.toUpperCase();
+});
+app.set('view engine', 'hbs');
+// app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now= new Date().toString();
+  var log= `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFileSync('server.log', log + '\n' );
+  next();
+})
+
+// app.use( (req, res, next) => {
+//   res.render('maintenance.hbs',{
+//     title: 'Maintenance'
+//   })
+// });
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs', {
+    name: 'Dushyant Surya',
+    pageTitle: 'Home Page',
+    message: 'Welcome to Home!'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Bad Gateway!!!!!'
+  })
+})
+
+app.listen(3000,() => {
+  console.log('Setting up server at port: 3000');
+});
